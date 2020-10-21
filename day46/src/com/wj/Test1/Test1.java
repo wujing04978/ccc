08fb417197183468bc4ca4a7d8d963e902f5418f
wj <wj@atstudy.com>
2020-10-21 15:50:06 +0800

    第一次提交

---
+++ day46/src/com/wj/Test1/Test1.java
@@ -0,0 +1,12 @@
+package com.wj.Test1;
+
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("aaa");
+        System.out.println("aaa");
+        System.out.println("aaa");
+        System.out.println("aaa");
+        System.out.println("aaa");
+        System.out.println("aaa");
+    }
+}
